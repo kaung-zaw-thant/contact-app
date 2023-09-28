cb5c0c9bc3ef7cd071be2c830955d0dd5c6055e6
kaungzawthant <kaungzawthant98@gmail.com>
2023-09-28 19:01:12 +0630

    create view files

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -3,6 +3,9 @@
 namespace Database\Seeders;
 
 // use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+
+use App\Models\Company;
+use App\Models\Contact;
 use Illuminate\Database\Seeder;
 
 class DatabaseSeeder extends Seeder
@@ -18,5 +21,7 @@ class DatabaseSeeder extends Seeder
         //     'name' => 'Test User',
         //     'email' => 'test@example.com',
         // ]);
+        Company::factory()->count(10)->create();
+        Contact::factory()->count(50)->create();
     }
 }
