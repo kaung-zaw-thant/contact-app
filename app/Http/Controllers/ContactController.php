64c58846503ac41eafd20c820fb66d48fe7f5d29
kaungzawthant <kaungzawthant98@gmail.com>
2023-09-29 21:49:52 +0630

    add view and CRUD process

---
+++ app/Http/Controllers/ContactController.php
@@ -0,0 +1,83 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Http\Controllers\Controller;
+use App\Models\Company;
+use App\Models\Contact;
+
+class ContactController extends Controller
+{
+    public function index()
+    {
+        return view('contacts.index', ['contacts' => Contact::orderBy('id', 'desc')->where(function ($query) {
+            if($companyId = request('company_id')){
+                $query->where('company_id', $companyId);
+            }
+
+            if($search = request('search')){
+                $query->where('first_name', 'LIKE', "%{$search}%");
+            }
+        })->paginate(10),
+        'companies' => Company::orderBy('name')->pluck('name','id')->prepend('All Companies', '')]);
+    }
+
+    public function create()
+    {
+        return view('contacts.create',['companies'=>Company::orderBy('name')->pluck('name','id')->prepend('All Companies', ''),
+                                        'contact'=>new Contact]);
+    }
+
+    public function store()
+    {
+        $request = request()->validate([
+            'first_name' => 'required',
+            'last_name' => 'required',
+            'email' => 'required | email',
+            'phone' => 'required',
+            'address' => 'required',
+            'company_id' => 'required | exists:companies,id'
+        ]);
+        Contact::create($request);
+
+        return redirect()->route('contacts.index')->with('message', "Contact has been added successfully");
+    }
+
+    public function edit($id)
+    {
+        return view('contacts.edit',['companies'=>Company::orderBy('name')->pluck('name','id')->prepend('All Companies', ''),
+                                    'contact'=> Contact::findOrFail($id)]);
+    }
+
+    public function update($id)
+    {
+        $request = request()->validate([
+            'first_name' => 'required',
+            'last_name' => 'required',
+            'email' => 'required | email',
+            'phone' => 'required',
+            'address' => 'required',
+            'company_id' => 'required | exists:companies,id'
+        ]);
+
+        $contact = Contact::findOrFail($id);
+        $contact->update($request);
+
+        return redirect()->route('contacts.index')->with('message', "Contact has been updated successfully");
+    }
+
+    public function show($id)
+    {
+        $contact = Contact::find($id);
+        return view('contacts.show', compact('contact'));
+    }
+
+    public function destroy($id)
+    {
+        $contact = Contact::findOrFail($id);
+        $contact->delete();
+
+        return back()->with('message', "Contact has been deleted successfully");
+    }
+
+}
--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\ContactController;
 use App\Models\Contact;
 use Illuminate\Support\Facades\Route;
 
@@ -18,15 +19,18 @@ Route::get('/', function () {
     return view('welcome');
 });
 
-Route::get('/contacts', function() {
-    return view('contacts.index');
-})->name('contacts.index');
+Route::get('/contacts', [ContactController::class, 'index'])->name('contacts.index');
 
-Route::get('/contacts/create', function () {
-    return view('contacts.create');
-})->name('contacts.create');
+Route::post('/contacts/store', [ContactController::class, 'store'])->name('contacts.store');
 
-Route::get('/contacts/{id}', function ($id) {
-    $contact = Contact::find($id);
-    return view('contacts.show', compact('contact'));
-})->name('contacts.show');
+Route::get('/contacts/create', [ContactController::class, 'create'])->name('contacts.create');
+
+Route::get('/contacts/{id}', [ContactController::class, 'show'])->name('contacts.show');
+
+Route::get('/contacts/{id}/edit', [ContactController::class, 'edit'])->name('contacts.edit');
+
+Route::put('/contacts/{id}', [ContactController::class, 'update'])->name('contacts.update');
+
+Route::delete('/contacts/{id}', [ContactController::class, 'destroy'])->name('contacts.destroy');
+
+
